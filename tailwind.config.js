a9d550a024af594b3417351893674e69a4cb7213
atlas <robertdennispersonal@gmail.com>
2019-08-16 20:00:20 -0400

    initial website

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+          'Montserrat',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: [
+        'Georgia',
+        'Cambria',
+        '"Times New Roman"',
+        'Times',
+        'serif',
+      ],
+      mono: [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
